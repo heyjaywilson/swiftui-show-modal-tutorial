0884d7fbc9d1a53eb8ad937fb18d6809f772be05
maeganwilson_ <maegan@maeganwilson.com>
2019-08-24 21:44:13 -0500

    Step 4

---
+++ modal-example/modal-example/ModalView.swift
@@ -0,0 +1,22 @@
+//
+//  ModalView.swift
+//  modal-example
+//
+//  Created by Maegan Wilson on 8/24/19.
+//  Copyright © 2019 MW. All rights reserved.
+//
+
+// 4: Create a new view for the modal
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is a modal")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
